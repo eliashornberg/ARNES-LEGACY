9e7b7b6344b6f477aaf5831fb918c41a0bf8f294
fabianbergstrom <@fabbe820>
2023-09-11 18:15:55 +0200

    ja e zlatan

--- Liu_progg/TDDD23/templar.gd
+++ Liu_progg/TDDD23/templar.gd
@@ -12,38 +12,56 @@ var screen_size # Size of the game window.
 func _ready():
 	screen_size = get_viewport_rect().size
 	_state_machine.travel("idle")
+	_animation_tree.active = true
 	
 func _process(delta):
+	var moving = false
 	var current = _state_machine.get_current_node()
+	if current != "roll-right" and current != "thanos-snapped":
+		$Sprite2D.flip_h = false
 	var velocity = Vector2.ZERO # The player's movement vector.
 	var looking_right = true
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+		moving = true
+		_state_machine.travel("run-down")
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+		moving = true
+		_state_machine.travel("run-up")
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
 		looking_right = true
-		if not current == "attack-right":
-			_state_machine.travel("run-sword")
+		moving = true
+		_state_machine.travel("run-sword")
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
 		looking_right = false
-		if not current == "attack-left":
-			_state_machine.travel("run-left")
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-		if not current == "attack-down":
-			_state_machine.travel("run-down")
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-		if not current == "attack-up":
-			_state_machine.travel("run-up")
+		moving = true
+		_state_machine.travel("run-left")
 	if Input.is_action_pressed("attack"):
+		moving = true
 		if looking_right:
 			_state_machine.travel("attack-right")
 		else:
 			_state_machine.travel("attack-left")
 	if Input.is_action_pressed("roll"):
-		_state_machine.travel("roll-right")
+		moving = true
+		if looking_right:
+			_state_machine.travel("roll-right")
+		else:
+			$Sprite2D.flip_h = true
+			_state_machine.travel("roll-right")
 	if Input.is_action_pressed("thanos"):
-		_state_machine.travel("thanos-snapped")
+		moving = true
+		if looking_right:
+			_state_machine.travel("thanos-snapped")
+		else:
+			$Sprite2D.flip_h = true
+			_state_machine.travel("thanos-snapped")
+	if not moving:
+		_state_machine.travel("idle")
+		
 	
 		
 
