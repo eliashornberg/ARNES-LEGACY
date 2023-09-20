a478092e543caf43ae3ebf59fb9def94aa5706ac
fabianbergstrom <@fabbe820>
2023-09-20 17:21:19 +0200

    lagt till scene switch

--- Liu_progg/TDDD23/game.gd
+++ Liu_progg/TDDD23/game.gd
@@ -6,7 +6,9 @@ var spawn_timer = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if global.justEnteredWorld:
+		$templar.position.x = 568
+		$templar.position.y = 441
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,3 +28,15 @@ func spawn_enemy():
 func _on_spawn_timer_timeout():
 	spawn_enemy()
 	spawn_timer = true
+
+
+func _on_bateman_area_2d_body_entered(body):
+	if body.is_in_group("hero"):
+		global.justEnteredBatemanHouse = true
+		if not global.justEnteredWorld:
+			get_tree().change_scene_to_file("res://bateman_house.tscn")
+
+
+func _on_bateman_area_2d_body_exited(body):
+	if body.is_in_group("hero"):
+		global.justEnteredWorld = false
--- Liu_progg/TDDD23/global.gd
+++ Liu_progg/TDDD23/global.gd
@@ -2,3 +2,5 @@ extends Node
 
 
 var player_attacking = false
+var justEnteredBatemanHouse = false
+var justEnteredWorld = false
